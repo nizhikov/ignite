f54c51e863665b170a6c59d6c57a9dd52caf3a13
Nikolay Izhikov <NIzhikov.dev@gmail.com>
2017-09-05 17:02:46 +0300

    IGNITE-3084: Prototype of Ignite DataFrame support. IgniteCatalog implementation. Some examples provided. There is only placeholer for an Ignite extra optimization and strategies.

---
+++ modules/spark/src/main/scala/org/apache/spark/sql/ignite/IgniteSharedState.scala
@@ -0,0 +1,13 @@
+package org.apache.spark.sql.ignite
+
+import org.apache.ignite.Ignite
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.catalyst.catalog.ExternalCatalog
+import org.apache.spark.sql.internal.SharedState
+
+/**
+  * Shared state to override link to IgniteExternalCatalog
+  */
+class IgniteSharedState(ignite: Ignite, sparkContext: SparkContext) extends SharedState(sparkContext) {
+    override lazy val externalCatalog: ExternalCatalog = new IgniteExternalCatalog(Some(ignite))
+}
